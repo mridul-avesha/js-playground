357d040bb440bd34d698ec303998d3c2821c70ab
Abhijai Singh <83866393+AbhijaiSingh151202@users.noreply.github.com>
2025-04-26 08:48:02 +0530

    meridiem

--- 6-clock/script.js
+++ 6-clock/script.js
@@ -1,15 +1,17 @@
 function getTime(){
     let now = new Date()
 
-    let hours = now.getHours().toString().padStart(2, '0')
+    let hours = now.getHours()
+    let meridiem = hours >= 12 ? "PM" : "AM"
+    hours = hours % 12 || 12
+    hours = hours.toString().padStart(2, '0')
+
     let minutes = now.getMinutes().toString().padStart(2, '0')
     let seconds = now.getSeconds().toString().padStart(2, '0')
 
-    return `${hours} : ${minutes} : ${seconds}`
+    return `${hours} : ${minutes} : ${seconds} ${meridiem}`
 }
 
 setInterval(function(){
     document.getElementById('clock').innerHTML = getTime()
 }, 1000)
-
-// hours>=12 ? "PM" : "AM"
